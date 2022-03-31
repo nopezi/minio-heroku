9a282ced7c30b55111ead3b59c2bdbcbd4d7b9f4
nopezi <nopezisaputra@gmail.com>
2022-03-31 15:49:49 +0700

    firsst

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+cd /minio
+wget https://dl.minio.io/server/minio/release/linux-amd64/minio
+chmod +x minio
+export MINIO_ACCESS_KEY="$ACCESS_KEY"
+export MINIO_SECRET_KEY="$SECRET_KEY"
+export PORT="$PORT"
+./minio server --address :$PORT /mnt/minio/data
